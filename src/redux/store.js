df91a1138938e1a733700d27c72322681f48b7af
armando.sanmiguel <armandosanmiguel@gmail.com>
2024-08-05 14:56:24 -0600

    Uso de ducks react-redux

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import {legacy_createStore as createStore, combineReducers, compose, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import pokeReducer from './pokeDucks'
+
+const rootReducer = combineReducers({
+    pokemones: pokeReducer
+});
+
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default function generateStore() {
+    const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+    return store;
+};
